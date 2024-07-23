e146d7f5492e90b5b17e0a5fdf6c068402b0d213
Jakub Gąsiorek <kubagasiorek6@gmail.com>
2024-07-23 13:00:36 +0200

    Added blank Dashboard page

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,10 @@
+
+const Dashboard = () => {
+  return (
+    <div className="text-white mt-10 pt-12">
+        <h1 className="flex-center text-3xl">Welcome to your dashboard</h1>
+    </div>
+  )
+}
+
+export default Dashboard
