1ba83934c3080423bb913310c64e534ba14017eb
Jakub Gąsiorek <kubagasiorek6@gmail.com>
2024-07-25 18:30:58 +0200

    Updated validation

--- lib/validation.ts
+++ lib/validation.ts
@@ -1,15 +1,24 @@
 import { z } from "zod";
 
-export const RegistrationFormValidation = z.object({
+export const RegistrationFormValidation = z
+  .object({
+    name: z.string().min(1, { message: "Name is required" }),
     email: z.string().email({ message: "Invalid email address" }),
-    password: z.string().min(6, { message: "Password must be at least 6 characters" }),
-    confirmPassword: z.string().min(6, { message: "Password must be at least 6 characters" }),
-  }).refine(data => data.password === data.confirmPassword, {
+    password: z
+      .string()
+      .min(6, { message: "Password must be at least 6 characters" }),
+    confirmPassword: z
+      .string()
+      .min(6, { message: "Password must be at least 6 characters" }),
+  })
+  .refine((data) => data.password === data.confirmPassword, {
     message: "Passwords do not match",
     path: ["confirmPassword"], // Field that will receive the error
   });
 
 export const UserFormValidation = z.object({
   email: z.string().email("Invalid email address"),
-  password: z.string().min(6, { message: "Password must be at least 6 characters" }),
+  password: z
+    .string()
+    .min(6, { message: "Password must be at least 6 characters" }),
 });
