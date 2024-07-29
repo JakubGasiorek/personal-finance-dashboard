a1e68d1b6a0d6e5925e358249d344885826cd404
Jakub Gąsiorek <kubagasiorek6@gmail.com>
2024-07-29 13:00:28 +0200

    Added hooks folder for more clarity

---
+++ hooks/useFinancialData.ts
@@ -0,0 +1,64 @@
+import { useState, useEffect } from "react";
+import { onAuthStateChanged, User } from "firebase/auth";
+import { collection, getDocs } from "firebase/firestore";
+import { auth, db } from "@/services/firebase";
+import { FinancialData, Income, Expense, Summary } from "@/types";
+
+const useFinancialData = (): FinancialData => {
+  const [financialData, setFinancialData] = useState<FinancialData>({
+    summary: [],
+    income: [],
+    expenses: [],
+  });
+
+  useEffect(() => {
+    const fetchData = async (user: User) => {
+      try {
+        const userId = user.uid;
+        const [summarySnapshot, incomeSnapshot, expensesSnapshot] =
+          await Promise.all([
+            getDocs(collection(db, `users/${userId}/summary`)),
+            getDocs(collection(db, `users/${userId}/income`)),
+            getDocs(collection(db, `users/${userId}/expenses`)),
+          ]);
+
+        const summaryData = summarySnapshot.docs.map((doc) => ({
+          id: doc.id,
+          ...doc.data(),
+        })) as Summary[];
+
+        const incomeData = incomeSnapshot.docs.map((doc) => ({
+          id: doc.id,
+          ...doc.data(),
+          date: doc.data().date ? doc.data().date.toDate() : new Date(),
+        })) as Income[];
+
+        const expensesData = expensesSnapshot.docs.map((doc) => ({
+          id: doc.id,
+          ...doc.data(),
+          date: doc.data().date ? doc.data().date.toDate() : new Date(),
+        })) as Expense[];
+
+        setFinancialData({
+          summary: summaryData,
+          income: incomeData,
+          expenses: expensesData,
+        });
+      } catch (error) {
+        console.error("Error fetching financial data:", error);
+      }
+    };
+
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        fetchData(user);
+      }
+    });
+
+    return () => unsubscribe();
+  }, []);
+
+  return financialData;
+};
+
+export default useFinancialData;
