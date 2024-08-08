59ce16dafd112d5fa96dd65b96933e1f817c8bd6
Jakub Gąsiorek <kubagasiorek6@gmail.com>
2024-08-08 14:36:17 +0200

    Added utility function withAuthenticatedUser

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,38 @@
+import { auth } from "@/services/firebase";
 import { type ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
+}
+
+type AuthenticatedAction = (userId: string) => Promise<void>;
+
+export async function withAuthenticatedUser(action: AuthenticatedAction) {
+  return new Promise<void>((resolve, reject) => {
+    const unsubscribe = auth.onAuthStateChanged(
+      async (user) => {
+        unsubscribe();
+
+        if (!user) {
+          console.error("User is not authenticated");
+          reject(new Error("User is not authenticated"));
+          return;
+        }
+
+        const userId = user.uid;
+        try {
+          await action(userId);
+          resolve();
+        } catch (error) {
+          console.error("Error performing authenticated action:", error);
+          reject(error);
+        }
+      },
+      (error) => {
+        console.error("Error checking authentication state:", error);
+        reject(error);
+      }
+    );
+  });
 }
