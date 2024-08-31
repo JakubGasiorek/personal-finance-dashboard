cf6df85be1c95dd0a14dbdf2bcec107220388637
Jakub Gąsiorek <kubagasiorek6@gmail.com>
2024-08-31 15:15:21 +0200

    Added option to display income chart from selected month and year or whole year

--- components/charts/IncomeChart.tsx
+++ components/charts/IncomeChart.tsx
@@ -17,17 +17,15 @@ ChartJS.register(BarElement, CategoryScale, LinearScale, Tooltip, Title);
 
 const IncomeChart: React.FC = () => {
   // State for selected month and year
-  const [selectedMonth, setSelectedMonth] = useState<number>(
-    new Date().getMonth()
-  );
+  const [selectedMonth, setSelectedMonth] = useState<number | "all">("all");
   const [selectedYear, setSelectedYear] = useState<number>(
     new Date().getFullYear()
   );
 
-  // Access income data from Redux store
+  // Access expense data from Redux store
   const incomeData = useSelector((state: RootState) => state.income.income);
 
-  // Define color map for categories
+  // Define color map for source
   const incomeColorMap: Record<string, string> = {};
   incomeData.forEach((incomeItem) => {
     getColorForCategory(incomeItem.source, incomeColorMap);
@@ -36,20 +34,47 @@ const IncomeChart: React.FC = () => {
   // Filter data by the specified month and year
   const filteredData = incomeData.filter((income) => {
     const date = new Date(income.date);
-    return (
-      date.getMonth() === selectedMonth && date.getFullYear() === selectedYear
-    );
+    return selectedMonth === "all"
+      ? date.getFullYear() === selectedYear
+      : date.getMonth() === selectedMonth &&
+          date.getFullYear() === selectedYear;
   });
 
-  // Prepare data for the chart
-  const labels = filteredData.map((income) => `${income.source}`);
-  const data = filteredData.map((income) => income.amount);
-  const backgroundColors = filteredData.map((income) =>
-    getColorForCategory(income.source || "Uncategorized", incomeColorMap)
-  );
-  const dates = filteredData.map((income) =>
-    new Date(income.date).toLocaleDateString()
-  );
+  let labels: string[],
+    data: number[],
+    backgroundColors: string[],
+    dates: string[];
+
+  if (selectedMonth === "all") {
+    // Group data by month for the whole year
+    const monthlyData = Array(12)
+      .fill(0)
+      .map(() => ({ total: 0, count: 0 }));
+    filteredData.forEach((income) => {
+      const month = new Date(income.date).getMonth();
+      monthlyData[month].total += income.amount;
+      monthlyData[month].count++;
+    });
+
+    labels = monthlyData.map((_, index) =>
+      new Date(0, index).toLocaleString("en-US", { month: "short" })
+    );
+    data = monthlyData.map((month) => month.total);
+    backgroundColors = filteredData.map((income) =>
+      getColorForCategory(income.source || "Uncategorized", incomeColorMap)
+    );
+    dates = labels;
+  } else {
+    // Use the original logic for a specific month
+    labels = filteredData.map((income) => `${income.source}`);
+    data = filteredData.map((income) => income.amount);
+    backgroundColors = filteredData.map((income) =>
+      getColorForCategory(income.source || "Uncategorized", incomeColorMap)
+    );
+    dates = filteredData.map((income) =>
+      new Date(income.date).toLocaleDateString()
+    );
+  }
 
   const chartData = {
     labels: labels,
@@ -72,27 +97,21 @@ const IncomeChart: React.FC = () => {
             const index = tooltipItem.dataIndex;
             const value = tooltipItem.raw as number;
             const date = dates[index];
-            return `${date}  $${value.toFixed(2)}`;
+            return selectedMonth === "all"
+              ? `${date}: $${value.toFixed(2)}`
+              : `${date}  $${value.toFixed(2)}`;
           },
         },
       },
     },
     scales: {
       x: {
-        ticks: {
-          color: "white",
-        },
-        grid: {
-          color: "rgba(255, 255, 255, 0.1)",
-        },
+        ticks: { color: "white" },
+        grid: { color: "rgba(255, 255, 255, 0.1)" },
       },
       y: {
-        ticks: {
-          color: "white",
-        },
-        grid: {
-          color: "rgba(255, 255, 255, 0.1)",
-        },
+        ticks: { color: "white" },
+        grid: { color: "rgba(255, 255, 255, 0.1)" },
       },
     },
     aspectRatio: 2,
@@ -105,22 +124,27 @@ const IncomeChart: React.FC = () => {
 
   return (
     <div className="flex flex-col items-center mb-4">
-      <div className="flex gap-4 mb-4">
+      <div className="flex gap-4 mb-4 w-full">
         <select
           value={selectedMonth}
-          onChange={(e) => setSelectedMonth(Number(e.target.value))}
-          className="p-2  bg-dark-300 text-white rounded"
+          onChange={(e) =>
+            setSelectedMonth(
+              e.target.value === "all" ? "all" : Number(e.target.value)
+            )
+          }
+          className="p-2 bg-dark-300 text-white rounded w-full"
         >
+          <option value="all">Whole year</option>
           {Array.from({ length: 12 }, (_, i) => (
             <option key={i} value={i}>
-              {new Date(0, i).toLocaleString("default", { month: "long" })}
+              {new Date(0, i).toLocaleString("en-US", { month: "long" })}
             </option>
           ))}
         </select>
         <select
           value={selectedYear}
           onChange={(e) => setSelectedYear(Number(e.target.value))}
-          className="p-2 bg-dark-300 text-white rounded"
+          className="p-2 bg-dark-300 text-white rounded w-full"
         >
           {years.map((year) => (
             <option key={year} value={year}>
